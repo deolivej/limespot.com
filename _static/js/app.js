38d37e12038ddb88f14e5474a94ecad6b77eaed8
deolivej <jdeolive@gmail.com>
2014-01-16 16:11:40 -0700

    Randomized order of slides in slide show, fixes #24.
    Also put functions into limespot namespace.

--- _static/js/app.js
+++ _static/js/app.js
@@ -1,25 +1,37 @@
-function init() {
-  // randomly choose background image
-  // var x = Math.floor(Math.random()*10);
-  // var banner = document.getElementById('ls-banner-img');
-  // if (banner != null) {
-  //   banner.setAttribute(
-  //     "src", x % 2 == 0 ? "img/bg-male.jpg" : "img/bg-female.jpg");
-  // }
-
-  // smooth scroll
-  $(function() {
-    $('a[href*=#][data-toggle!="tab"]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
+window.limespot = {
+  /*
+   * initialization function called for every page body.onload
+   */
+  init: function() {
+    // set up smooth scrolling
+    $(function() {
+      $('a[href*=#][data-toggle!="tab"]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
         }
-      }
+      });
     });
-  });
+  },
+
+  /*
+   * utility function to randomize an array, taken from:
+   * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+   */
+  shuffle: function(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+  }
 }
+
